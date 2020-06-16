282d7653db6df1e1fde942b635e7449ed3de2b0e
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-16 15:44:32 +0000

    Ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -34,40 +34,38 @@ initial
 
 //Todo: User logic
 initial begin 
-  rst <= 1;
-  enable <= 0;
+  rst <= 0;
+  enable <= 1;
   direction  <= 1;
   err <= 0;
-  #(CLK_PERIOD)
-  rst <= 0;
-  #(CLK_PERIOD)
-  enable <= 1;
-  counter_out <= 8'b10;
- 
- 	forever begin
+  #CLK_PERIOD
+  #CLK_PERIOD
+   
+       forever begin
          #CLK_PERIOD
          if ((counter_out!==8'b00000011)begin
-           if (counter_out==8'b00000000)begin
-             $display("***TEST FAILED! rst ==%d, counter is meant to reset when rst=1***",rst[0]);
-             err=1;
+         	if (counter_out==8'b00000000)begin
+          	   $display("***TEST FAILED! rst ==%d, counter is only meant to reset when rst=1***",rst);
+          	   err=1;
 		end 
-            else if (counter_out == 8'b00000001)begin
-           $display("***TEST FAILED! Counting backwards output is %b. expected output is 8'b00000011",counter_out);
-           err=1;
-         end
-         else if (counter_out == 8'b00000010)begin
-           $display("***TEST FAILED! enable is %d yet output stays the same",enable);
-           err=1;
-         end
-	else begin
-           $display("***TEST FAILED! out is %b. Have you written any code yet?***",counter_out);
-           err=1;
-         end
-       end
-	else begin 
-	err = 0;
-	end 
-     end
+          	else if (counter_out == 8'b00000001)begin
+          	   $display("***TEST FAILED! Counting backwards output is %b. expected output is 8'b00000011",counter_out);
+          	   err=1;
+         	end
+         	else if (counter_out == 8'b00000010)begin
+           	   $display("***TEST FAILED! enable is %d yet output stays the same",enable);
+           	   err=1;
+         	end
+	        else begin
+           	   $display("***TEST FAILED! out is %b. Have you written any code yet?***",counter_out);
+           	   err=1;
+         	end
+          else begin
+		err = 0;
+		end
+	  end 
+      end
+end 
 //Todo: Finish test, check for success
 
 	initial begin
