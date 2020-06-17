e81a250a02036f4746a75a88002a5ed3a2899c20
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-17 12:07:48 +0000

    Incomplete Ex5 Solution

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,27 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module traffic(input clk,
+	output red
+	output amber 
+	output green);
+
+reg [2:0]state 
+
+assign red = state[2];
+assign amber = state[1];
+assign green = state[0];
+
+always @(posedge clk)
+	case (state) 
+		3'b100: state<= 3'b110;
+		3'b110: state<= 3'b001;
+		3'b001: state<= 3'b010;
+		3'b010: state<= 3'b100;
+		default: state<= 3'b001;
+	endcase
+endmodule 
+
+		
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,119 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+parameter CLK_PERIOD = 10;
+
+
+//Todo: Regitsers and wires
+reg clk;
+wire red;
+wire yellow;
+wire amber;
+reg state;
+
+//Todo: Clock generation
+initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Todo: User logic
+forever begin
+	state<=3'b100
+	#(CLK_PERIOD)
+         if (throw!==3'b110)begin
+          	   $display("***RED TO AMBER GREEN TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b110
+	#(CLK_PERIOD)
+         if (throw!==3'b001)begin
+          	   $display("*** AMBER GREEN TO GREEN TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b001
+	#(CLK_PERIOD)
+         if (throw!==3'b010)begin
+          	   $display("***GREEN TO AMBER TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b010
+	#(CLK_PERIOD)
+         if (throw!==3'b100)begin
+          	   $display("***AMBER TO RED TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b000
+	#(CLK_PERIOD)
+         if (throw!==3'b001)begin
+          	   $display("***ILLEGAL STATE TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+#CLK_PERIOD
+	state<=3'b101
+	#(CLK_PERIOD)
+         if (throw!==3'b001)begin
+          	   $display("***ILLEGAL STATE TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b011
+	#(CLK_PERIOD)
+         if (throw!==3'b001)begin
+          	   $display("***ILLEGAL STATE TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+	#CLK_PERIOD
+	state<=3'b111
+	#(CLK_PERIOD)
+         if (throw!==3'b001)begin
+          	   $display("***ILLEGAL STATE TEST 1 FAILED!***");
+          	   err=1;
+		end 
+	else 
+		   err = 0;
+
+end 
+//Todo: Finish test, check for success
+
+	initial begin
+        #150 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate counter module
+	dice top (.clk(clk),
+		.red(red),
+		.amber(amber),
+		.yellow(yellow));
+ 
+endmodule
+
